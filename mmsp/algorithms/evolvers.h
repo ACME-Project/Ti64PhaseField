101af58e5634eacee38c117646d5bb4d8d90307d
ArunBaskaran <arupad@gmail.com>
2021-03-26 22:35:47 -0400

    Add files via upload

---
+++ mmsp/algorithms/evolvers.h
@@ -0,0 +1,9 @@
+// evolvers.hpp
+// Definitions of methods to evolve MMSP grids
+#ifndef _EVOLVERS_H_
+#define _EVOLVERS_H_
+template<int dim, typename T>
+void interface_field_evolves(const T& dt, const T& width, const T& gamma, const T& epsilon, const T& w, const T& mu,
+														 const MMSP::grid<dim,MMSP::sparse<T> >& former, MMSP::grid<dim,MMSP::sparse<T> >& latter, const int node);
+
+#endif // _EVOLVERS_H_
